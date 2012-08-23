61e98eb1ea558db5e0cf32a6237d51f95eec7b6e
Yogthos <dmitri.sotnikov@gmail.com>
2012-08-23 15:28:16 -0300

    Update src/ga/main.clj

--- src/ga/main.clj
+++ src/ga/main.clj
@@ -15,33 +15,47 @@
          :fitness (fitness value target)})
       member)))
 
-(defn- rank [population]
+(defn- rank
+  "ranks the population by fitness"
+  [population]
   (reverse (sort-by :fitness population)))
 
-(defn- update-vals [fitness target {v1 :value} {v2 :value}]  
+(defn- update-vals
+  "randomly selects a value from either the first or the second memeber for each position"
+  [fitness target {v1 :value} {v2 :value}]  
   (let [value (map #(if (> (rand) 0.5) %1 %2) v1 v2)]
     {:value value :fitness (fitness value target)}))
 
-(defn- mate [population fitness target]
+(defn- mate
+  "splits the population in half and randomly mates all the members"
+  [population fitness target]
   (apply map 
          (partial update-vals fitness target) 
-         (split-at (/ (count population) 2) population)))
+         (split-at (/ (count population) 2) (shuffle population))))
 
-(defn- evolve-step [size population mutator threshold fitness target]
+(defn- evolve-step
+  "mutate the population, then promote top members and add mated members to the end"
+  [size population mutator threshold fitness target]
   (let [mutated (rank (mutate population mutator threshold fitness target))        
         promote-size (/ size 5)
         keep-size (- (/ size 2) promote-size)
         [xs ys] (split-at keep-size mutated)]
     (concat xs (take promote-size ys) (mate mutated fitness target))))
 
-(defn- gen-member [mutator fitness target]
+(defn- gen-member
+  "generates a new member for the population"
+  [mutator fitness target]
   (let [value (take (count target) (repeatedly #(mutator nil)))] 
     {:value value :fitness (fitness value target)}))
 
-(defn- init-population [size mutator fitness target]
+(defn- init-population
+  "creates a new population"
+  [size mutator fitness target]
   (rank (take size (repeatedly #(gen-member mutator fitness target)))))
 
-(defn evolve [size threshold mutator fitness target]
+(defn evolve
+  "evolves the population until at least one member is fit"
+  [size threshold mutator fitness target]
   (loop [population (init-population size mutator fitness target)]
     (println (first population))
     (if (zero? (:fitness (first population))) 
