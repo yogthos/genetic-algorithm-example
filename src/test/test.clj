750f3a917907db74a46492eea9729b3b8959c201
Yogthos <dmitri.sotnikov@gmail.com>
2012-08-23 12:26:32 -0300

    Update src/test/test.clj

--- src/test/test.clj
+++ src/test/test.clj
@@ -3,16 +3,15 @@
   (:gen-class )) 
   
 ;example usage
-(defn -main [args]
-  (let [target (vec "Hello World!")
-  			mutator (struct ga/mutator-struct (fn[_] (char (ga/rand-in-range 32 126))) {})]
-    (defn get-fitness 
-      "custom comparator function for evaluating fitness of members"
-      [value target]
-      (reduce - 0 (map #(if (= (first %1) (second %1)) 0 1) 
-        (ga/zip value target))))       
-         
-    ;run the evolution function and print the result  
-    (time (println (apply str (:value @(first (ga/evolve 500 0.01 target mutator get-fitness))))))))
-  
-(-main nil)
+(defn -main [string]
+  (let [target  (vec string)
+        mutator (fn[_] (char (rand-in-range 32 126))) 
+        fitness (fn [target value]
+                  (reduce 
+                    (fn [rank [a b]] (if (= a b) rank (dec rank)))
+                    0 (map vector value target)))]
+    
+    (first (evolve 1000 0.01 target mutator fitness))   
+    (time (println (apply str (:value (first (evolve 500 0.01 target mutator fitness))))))))
+
+(-main "Hello World!")
