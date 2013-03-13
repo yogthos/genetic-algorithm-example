1282ad073656ce1d321153c4cd9af090a364516a
Yogthos <yogthos@Yogthoss-MacBook.local>
2013-03-13 07:07:44 -0400

    added project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj-ga "0.1.0-SNAPSHOT"
+  :description "a simple Tetris game"
+  :url "https://github.com/yogthos/Clojure-Genetic-Algorithm-Example"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  
+  :dependencies [[org.clojure/clojure "1.5.1"]])
